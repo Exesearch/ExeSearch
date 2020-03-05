e49e9084467c92cb9a483b17487fd678c2ed5996
JacobTeo <57627129+JacobTeo@users.noreply.github.com>
2020-03-05 11:52:46 +0000

    Update register.php

--- HTML/register.php
+++ HTML/register.php
@@ -28,7 +28,7 @@ if(isset($_POST['submit'])){
 	 // first check the database to make sure 
 	// a user does not already exist with the same username and/or email
 	$user_check_query = "select * from users WHERE username='$username' OR email='$email' LIMIT 1";
-	$result = mysqli_query($db, $user_check_query);
+	$result = mysqli_query($conn, $user_check_query);
 	$user = mysqli_fetch_assoc($result);
   
 	if ($user) { // if user exists
