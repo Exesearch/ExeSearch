11046cb0018e834bdcc2b14d08106dbc2a6e655c
yashhh9 <60701003+yashhh9@users.noreply.github.com>
2020-03-05 15:32:02 +0000

    Update index.php

--- HTML/index.php
+++ HTML/index.php
@@ -17,14 +17,26 @@
     <div class="form">
 	  <!--form for registration form-->
         <form class= "register-form" action= "register.php" method = "POST">
-            <input type="text" name ="forename" placeholder = "Forename" id= "forename" required>
-            <input type="text" name="surname" placeholder = "Surname" id="surname" required>
+
             <input type="text" name="username" placeholder = "Username" id="username" required>
             <input type="password" name="password" placeholder = "Password" id="password" required>
             <input type="email" name="email" placeholder = "Email" id="email" required>
+		
             <!--to account for three different users-->
-			<input type="text"  name="occupation" placeholder = "Occupation" id="occupation" required>
-			<!--button that will enter the credientials into database-->
+	    <select name="occupation" onchange="yesnoCheck(this)">
+                <option value="Student">Student</option>
+                <option value="Tutor">Tutor</option>
+                <option value="Gamemaster">Gamemaster</option>
+            </select>
+		
+		<div id="ifYes" style="display: none;">
+                <br/>
+                <input type="text" name ="private_key" placeholder="Private Key"/>
+            </div>
+
+            </br>
+            </br>
+		<!--button that will enter the credientials into database-->
             <button type="submit" name="submit" value= "Submit">Submit</button>
 			<!--this will take user to login form if they are already registered-->
             <p class="message">Already Registered? <a href="#">Login</a></p>
@@ -34,7 +46,7 @@
         <form class="login-form" action= "register.php" method= "POST">
             <input type="text" name="username" placeholder = "Username" id="username" required>
             <input type="password" name="password" placeholder = "Password" id="password" required>
-			<button type="submit" name="login_user" value= "Submit">Login</button>
+	    <button type="submit" name="login_user" value= "Submit">Login</button>
             <p class="message">Not Registered? <a href="#">Register</a></p>
         </form>
 
@@ -46,6 +58,15 @@
             $('.message a').click(function(){
                 $('form').animate({height:"toggle",opacity:"toggle"},"slow");
             });
+		
+		function yesnoCheck(that) {
+                if (that.value == "Tutor" || that.value == "Gamemaster") {
+
+                    document.getElementById("ifYes").style.display = "block";
+                } else {
+                    document.getElementById("ifYes").style.display = "none";
+                }
+            }
         </script>
 
     </div>
