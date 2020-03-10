a015adf9e6cf7d26881fb5121b2434318fec54da
Nell-Mills <59917132+Nell-Mills@users.noreply.github.com>
2020-03-10 17:39:06 +0000

    Update quiz.php

--- HTML/quiz.php
+++ HTML/quiz.php
@@ -50,8 +50,8 @@ if ($lresult->num_rows>0) {
 
 <nav id="navigationBar">
   <ul>
-    <li class="profile-icon"><a href="profile.html">Profile</a></li>
-    <li class="quiz-icon"><a href="quiz.html">Quiz</a></li>
+    <li class="profile-icon"><a href="profile.php">Profile</a></li>
+    <li class="quiz-icon"><a href="quiz.php">Quiz</a></li>
     <li class="scoreb-icon"><a href="scoreboard.php">Scoreboard</a></li>
     <li class="faq-icon"><a href="FAQ.html">FAQ</a></li>
   </ul>
