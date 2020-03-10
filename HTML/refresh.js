8d9b7dc575238faf429b67416dccbfd1511c01a1
Nell-Mills <59917132+Nell-Mills@users.noreply.github.com>
2020-03-10 16:18:01 +0000

    Update refresh.js

--- HTML/refresh.js
+++ HTML/refresh.js
@@ -1,9 +1,21 @@
 //Code to refresh the page if the game progress changes
 //Written by: Nell
-function refreshPage(currentQuestion, nextQuestion) {
-	if(currentQuestion != nextQuestion) {
-		currentQuestion = nextQuestion;
-		location.reload(true);
+<script>
+	function refreshPage(currentQuestion) {
+		var xhttp;
+		var nextQuestion = 1;
+		xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if(this.readyState === 4 && this.status === 200) {
+				nextQuestion = this.responseText;
+			}
+		}
+		xhttp.open("GET", "next_question.php", true);
+		xhttp.send();
+		if(currentQuestion != nextQuestion) {
+			currentQuestion = nextQuestion;
+			location.reload(true);
+		}
+		setTimeout("refreshPage()", 5000); //Run every 5 seconds.
 	}
-	setTimeout("refreshPage()", 5000); //Run every 5 seconds.
-}
+</script>
