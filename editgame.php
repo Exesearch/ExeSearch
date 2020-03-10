bff60799e16a461e5973ecb8af72ff7ecae9d646
JacobTeo <57627129+JacobTeo@users.noreply.github.com>
2020-03-10 14:06:17 +0000

    Update and rename blah.php to editgame.php

--- editgame.php
+++ editgame.php
@@ -1,16 +1,20 @@
 
-<?php require("./connection.php"); $qnss = [1,2,3,4,5];
+<?php require("./connection.php"); $qnss = [1,2,3,4,5,6];
   if(isset($_POST['submit'])){
     foreach($qnss as $qn){
       $qns = $_POST['qns'.$qn];
       $ans = $_POST['ans'.$qn];
-      $sqlq = "INSERT INTO testgm(questions, answers) VALUES ('$qns','$ans');";
+      $pt = $_POST['pt'.$qn];
+      $sqlq = "INSERT INTO questions(question, answer, points) VALUES ('$qns','$ans','$pt');";
       $sql = mysqli_query($conn,$sqlq);
       }
 
       foreach($qnss as $qn){
         $locationval = $_POST['location_'.$qn];
-        if($locationval === 'Harrisons'){
+        if($locationval === NULL){
+          echo "Input location";
+        }
+        elseif($locationval === 'Harrisons'){
           $sqllq = "UPDATE locations SET locid = $qn WHERE locname = 'Harrisons';";
         }else if ($locationval === 'Cornwall'){
           $sqllq = "UPDATE locations SET locid = $qn WHERE locname = 'Cornwall';";
@@ -20,7 +24,12 @@
            $sqllq = "UPDATE locations SET locid = $qn WHERE locname = 'Northcott';";
          }else if ($locationval === 'Greathall'){
            $sqllq = "UPDATE locations SET locid = $qn WHERE locname = 'Greathall';";
+         }else if ($locationval === 'Amory'){
+           $sqllq = "UPDATE locations SET locid = $qn WHERE locname = 'Amory';";
+         }else{
+           $sqllq = "";
          }
+
 
          echo $locationval;
          if (!$sqllq){
@@ -35,4 +44,11 @@
 
   mysqli_close($conn);
 
- } ?>
+ }
+ if (isset($_POST['reset'])){
+   foreach($qnss as $qn){
+     $sqlquery = "UPDATE locations SET locid = 9"."$qn WHERE locid = $qn;";
+     $sql = mysqli_query($conn,$sqlquery);
+ }
+}
+?>
