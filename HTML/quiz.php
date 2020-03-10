633d5448c8b1ceda82dbcb43d0301c44e123eb4b
Nell-Mills <59917132+Nell-Mills@users.noreply.github.com>
2020-03-10 16:19:00 +0000

    Update quiz.php

--- HTML/quiz.php
+++ HTML/quiz.php
@@ -39,7 +39,6 @@ if ($lresult->num_rows>0) {
 
 	<title>ExeSearch</title>
 	<link rel="stylesheet" href="style.css">
-	<script src="refresh.js"></script>
 
 </head>
 <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
