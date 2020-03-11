1adfa04c230cbb839b5591bbec3071cc96386f05
Nell-Mills <59917132+Nell-Mills@users.noreply.github.com>
2020-03-11 20:48:48 +0000

    Update create_progress.php

--- HTML/create_progress.php
+++ HTML/create_progress.php
@@ -3,7 +3,7 @@
 <?php
 	$game_name = $_GET['g'];
 	$teamid = $_GET['t'];
-	$connection = mysqli_connect("emps-sql.ex.ac.uk,"yk326","yk326","yk326","3306");
+	$connection = mysqli_connect('emps-sql.ex.ac.uk','yk326','yk326','yk326','3306');
 	$team_query = "ALTER TABLE $game_name ADD $teamid VARCHAR(50) SET DEFAULT 'FALSE'";
 	$team_result = mysqli_query($connection, $team_query);
 	$team_query_2 = "UPDATE $game_name SET $teamid = TRUE WHERE question_id = 1";
