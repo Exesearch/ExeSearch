c89b4738561f6c5fd49b005101ca01f8db8c6476
Nell-Mills <59917132+Nell-Mills@users.noreply.github.com>
2020-03-11 12:05:49 +0000

    Update next_question.php

--- HTML/next_question.php
+++ HTML/next_question.php
@@ -3,7 +3,7 @@
 <?php
 	require_once("configure.php");
 	$connection = mysqli_connect(server,user,pass,database,"3306");
-	$next_question_query = "SELECT quid FROM Teams WHERE teamid = userteamid LIMIT 1";
+	$next_question_query = "SELECT quid FROM $game_name WHERE teamid = TRUE";
 	$next_question_result = mysqli_query($connection, $next_question_query);
 	$next_question = fetch_assoc($next_question_result);
 	echo $next_question;
