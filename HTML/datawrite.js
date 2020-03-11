9310db96cc67b7ee921f96cda8e684e1ef3d6998
Nell-Mills <59917132+Nell-Mills@users.noreply.github.com>
2020-03-11 18:06:22 +0000

    Update datawrite.js

--- HTML/datawrite.js
+++ HTML/datawrite.js
@@ -1,16 +1,14 @@
-//Code to refresh the page if the game progress changes
+//Code to test AJAX - along with data_write.php and page.html
 //Written by: Nell
-<script>
-	function writedata() {
-		var xhttp;
-		xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function() {
-			if(this.readyState === 4 && this.status === 200) {
-				alert("Request Ready");
-			}
+function writedata() {
+	var xhttp;
+	xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if(this.readyState === 4 && this.status === 200) {
+			alert("Request Ready");
 		}
-		xhttp.open("GET", "data_write.php", true);
-		xhttp.send();
-		alert("Request Sent");
 	}
-</script>
+	xhttp.open("GET", "data_write.php", true);
+	xhttp.send();
+	alert("Request Sent");
+}
