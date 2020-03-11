0d1df5c6e630bb0dd205c7aa1d8312737d0fb62b
Nell-Mills <59917132+Nell-Mills@users.noreply.github.com>
2020-03-11 18:13:39 +0000

    Delete data_write.php

--- HTML/data_write.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$connection = mysqli_connect("emps-sql.ex.ac.uk,"yk326","yk326","yk326","3306");
-	$next_question_query = "INSERT INTO Users(uname,passwd,email,occupation) VALUES("TESTUSER","rat","rat@hotmail.com","Student");";
-	$next_question_result = mysqli_query($connection, $next_question_query);
-?>
