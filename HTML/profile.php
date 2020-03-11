68d5510644f7264c5b25c1a9a08f337fd8324224
yashhh9 <60701003+yashhh9@users.noreply.github.com>
2020-03-11 12:40:18 +0000

    Update profile.php

--- HTML/profile.php
+++ HTML/profile.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+$conn = mysqli_connect("emps-sql.ex.ac.uk","yk326","yk326","yk326","3306");
+
 // Check user login or not
 if(!isset($_SESSION['username'])){
     header('Location: profile.php');
@@ -26,7 +28,6 @@ if(!isset($_SESSION['username'])){
 <nav>
     <ul>
         <li class="profile-icon"><a href="profile.php">Profile</a></li>
-        <li class="quiz-icon"><a href="quiz.php">Quiz</a></li>
         <li class="scoreb-icon"><a href="scoreboard.php">Scoreboard</a></li>
         <li class="faq-icon"><a href="FAQ.html">FAQ</a></li>
     </ul>
@@ -38,7 +39,78 @@ if(!isset($_SESSION['username'])){
     <div id="center001">
         <div id ="color001">
 
-            <form class="profile1" action="change_username.php" method="POST">
+            <label for="group_name">You are in group: <?php
+
+            $username = $_SESSION["username"];
+
+            $result = mysqli_query($conn,"SELECT groupName FROM team WHERE member1='$username' OR member2='$username' OR member3='$username'
+                    OR member4='$username' OR member5='$username' OR member6='$username' OR member7='$username' OR member8='$username'
+                    OR member9='$username' OR member10='$username';");
+
+            $row = mysqli_fetch_assoc($result);
+            $group_name=$row["groupName"];
+
+            if(!empty($group_name)){
+
+                echo $group_name;
+
+            }else{
+
+                echo "-";
+            }
+
+
+            ?>
+            </label>
+
+            <form class="your_games" action="profile_action.php" method="POST">
+
+                </br>
+                <label>Press ready when you are at your meeting location for the game. The start button will be enabled
+                    when at least 50% of your team members have clicked the ready button and are at the meeting location.
+                    You may view your progress of each game by using the "View Progress" button.</label>
+                </br>
+                </br>
+                <label>Your Games</label>
+
+                <div class="game_progress">
+
+                    <?php
+
+                    $query0 = "SELECT * FROM games;";
+                    $do_query0 = mysqli_query($conn,$query0);
+
+                    if(mysqli_num_rows($do_query0)==0){
+
+                        echo 'No games have been created by the Game Master yet.';
+
+                    }elseif(mysqli_num_rows($do_query0)>0){
+                        while($row = mysqli_fetch_assoc($do_query0)){
+
+                            echo $row['game_name']." ";
+                            echo '<button type="button" name="ready" action="profile_action.php">Ready</button>';
+
+                            
+                            echo '<button type="button" disabled name="start">Start</button>';
+                            echo '<button type="button" disabled name="progress">View Progress</button>';
+
+                            echo '<label> Meeting location: </label>';
+                            echo $row['meeting_location'];
+                            echo '</br>';
+                        }
+                    }
+                    ?>
+
+
+                </div>
+
+            </form>
+
+
+
+            <form class="profile1" action="profile_action.php" method="POST">
+
+                </br>
 
                 <label for="username">Username</label>
                 </br>
@@ -46,73 +118,51 @@ if(!isset($_SESSION['username'])){
                 <input type="text" id="username" name="username" placeholder="New Username">
                 <input type="submit" name="save" value="Save Changes">
 
-
-
                 </br>
                 </br>
             </form>
 
-            <form name="frmChange" method="POST" action="change_password.php" onSubmit="return validatePassword()">
-                <div style="width:500px;">
-                    <div class="message"><?php if(isset($message)) { echo $message; } ?></div>
-                    <table border="0" cellpadding="10" cellspacing="0" width="500" align="center" class="tblSaveForm">
-                        <tr class="tableheader">
-                            <td colspan="2">Change Password</td>
-                        </tr>
-                        <tr>
-                            <td width="40%"><label>Current Password</label></td>
-                            <td width="60%"><input type="password" name="currentPassword" class="txtField"/><span id="currentPassword"  class="required"></span></td>
-                        </tr>
-                        <tr>
-                            <td><label>New Password</label></td>
-                            <td><input type="password" name="newPassword" class="txtField"/><span id="newPassword" class="required"></span></td>
-                        </tr>
-                        <td><label>Confirm Password</label></td>
-                        <td><input type="password" name="confirmPassword" class="txtField"/><span id="confirmPassword" class="required"></span></td>
-                        </tr>
-                        <tr>
-                            <td colspan="2"><input type="submit" name="save2" value="Save Changes" class="btnSubmit"></td>
-                        </tr>
+            <form name="frmChange" method="POST" action="profile_action.php" onSubmit="return validatePassword()">
+                <div class="message"><?php if(isset($message)) { echo $message; } ?></div>
 
-                        <script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
+                <label for="username">Change Password</label>
+                </br>
+                <input type="password" id="currentPassword" name="currentPassword" placeholder="Current Password" class="txtField" required/>
+                </br>
+                <input type="password" id="newPassword" name="newPassword" placeholder="New Password" class="txtField" required/>
+                </br>
+                <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm Password" class="txtField" required/>
+                <input type="submit" name="save2" value="Save Changes" class="btnSubmit">
 
-                        <script>
-                            function validatePassword() {
-                                var currentPassword,newPassword,confirmPassword,output = true;
-
-                                currentPassword = document.frmChange.currentPassword;
-                                newPassword = document.frmChange.newPassword;
-                                confirmPassword = document.frmChange.confirmPassword;
-
-                                if(!currentPassword.value) {
-                                    currentPassword.focus();
-                                    document.getElementById("currentPassword").innerHTML = "required";
-                                    output = false;
-                                }
-                                else if(!newPassword.value) {
-                                    newPassword.focus();
-                                    document.getElementById("newPassword").innerHTML = "required";
-                                    output = false;
-                                }
-                                else if(!confirmPassword.value) {
-                                    confirmPassword.focus();
-                                    document.getElementById("confirmPassword").innerHTML = "required";
-                                    output = false;
-                                }
-                                if(newPassword.value != confirmPassword.value) {
-                                    newPassword.value="";
-                                    confirmPassword.value="";
-                                    newPassword.focus();
-                                    document.getElementById("confirmPassword").innerHTML = "not same";
-                                    output = false;
-                                }
-                                return output;
-                            }
-                        </script>
-                    </table>
-                </div>
             </form>
 
+            </br>
+
+
+            <?php
+            session_start();
+            // Check user login or not
+            $conn = mysqli_connect("emps-sql.ex.ac.uk","yk326","yk326","yk326","3306");
+
+            $current_username = $_SESSION['username'];
+            $result = mysqli_query($conn, "SELECT occupation FROM Users WHERE uname='$current_username'");
+            $row = mysqli_fetch_array($result);
+
+            if($row['occupation'] == "Tutor"){
+
+                echo '<form action="tutor.php"><input type="submit" value="Tutor Mode" /></form>';
+
+            } elseif($row['occupation'] == "Gamemaster"){
+
+                echo '<form action="gamemaster.php"><input type="submit" value="Gamemaster Mode" /></form>';
+
+            }
+
+            ?>
+
+
+
+            </br>
             <a href="logout.php">Logout</a>
 
         </div>
@@ -123,5 +173,3 @@ if(!isset($_SESSION['username'])){
 
 </body>
 </html>
-
-
