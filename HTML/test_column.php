43d6bfe677e99e9e9f048a71411cbbe3afd66a83
Nell-Mills <59917132+Nell-Mills@users.noreply.github.com>
2020-03-11 20:02:23 +0000

    Update test_column.php

--- HTML/test_column.php
+++ HTML/test_column.php
@@ -1,1 +1,15 @@
-
+//Tests if a column exists in the table
+//Written by: Nell
+<?php
+	$game_name = $_GET['g'];
+	$teamid = $_GET['t'];
+	$connection = mysqli_connect("emps-sql.ex.ac.uk,"yk326","yk326","yk326","3306");
+	$column_query = "SELECT * FROM $game_name WHERE group_id = $teamid;";
+	$column_result = mysqli_query($connection, $column_query);
+  $column_count = mysqli_num_rows($column_result);
+  if($column_count > 0) {
+    echo "FALSE";
+  } else {
+    echo "TRUE";
+  }
+?>
