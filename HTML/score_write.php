be961c1ea1c4999d71d02ce39273dd8ab8cff547
Nell-Mills <59917132+Nell-Mills@users.noreply.github.com>
2020-03-11 18:45:32 +0000

    Add files via upload

---
+++ HTML/score_write.php
@@ -0,0 +1,7 @@
+<?php
+	$teamid = $_GET['t'];
+	$scoreval = $_GET['s'];
+	$connection = mysqli_connect("emps-sql.ex.ac.uk,"yk326","yk326","yk326","3306");
+	$score_query = "UPDATE team SET score = score + $scoreval WHERE group_id = $teamid;";
+	$score_result = mysqli_query($connection, $score_query);
+?>
