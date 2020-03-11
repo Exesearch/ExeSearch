bfcfde3d8673920ad09517cec5197b62e0bcac5c
Nell-Mills <59917132+Nell-Mills@users.noreply.github.com>
2020-03-11 19:03:01 +0000

    Update score_write.php

--- HTML/score_write.php
+++ HTML/score_write.php
@@ -1,4 +1,5 @@
-//Writes the data sent through the GET request to the database
+/*Writes the data sent through the GET request to the database
+  and increments the game progress counter.*/
 //Written by: Nell
 <?php
 	$teamid = $_GET['t'];
@@ -6,4 +7,12 @@
 	$connection = mysqli_connect("emps-sql.ex.ac.uk,"yk326","yk326","yk326","3306");
 	$score_query = "UPDATE team SET score = score + $scoreval WHERE group_id = $teamid;";
 	$score_result = mysqli_query($connection, $score_query);
+	
+	$game_name = $_GET['g'];
+	$current_question = $_GET['q'];
+	$next_question = $current_question + 1;
+	$progress_query = "UPDATE $game_name SET completed = FALSE WHERE question_id = $current_question;";
+	$progress_result = mysqli_query($connection, $progress_query);
+	$progress_query_2 = "UPDATE $game_name SET completed = TRUE WHERE question_id = $next_question;";
+	$progress_result_2 = mysqli_query($connection, $progress_query_2);
 ?>
