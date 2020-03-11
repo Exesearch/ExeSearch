8f3996e1b60d6dab9f5778b7a24ef33153c34b38
Nell-Mills <59917132+Nell-Mills@users.noreply.github.com>
2020-03-11 11:56:52 +0000

    Update next_question.php

--- HTML/next_question.php
+++ HTML/next_question.php
@@ -1,7 +1,10 @@
+//Code to query the database for game progress
+//Written by: Nell
 <?php
-	$connection = mysqli_connect("emps-sql.ex.ac.uk,"yk326","yk326","yk326","3306");
+	require_once("configure.php");
+	$connection = mysqli_connect(server,user,pass,database,"3306");
 	$next_question_query = "SELECT quid FROM Teams WHERE teamid = userteamid LIMIT 1";
 	$next_question_result = mysqli_query($connection, $next_question_query);
 	$next_question = fetch_assoc($next_question_result);
-	eco $next_question;
+	echo $next_question;
 ?>
