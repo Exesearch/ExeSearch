46da24a702458606338d4301c04f6a46d369d23d
Nell-Mills <59917132+Nell-Mills@users.noreply.github.com>
2020-03-11 18:52:03 +0000

    Update scorewrite.js

--- HTML/scorewrite.js
+++ HTML/scorewrite.js
@@ -1,6 +1,11 @@
-t s
-//Code to get a php script to increment the score in the database
+//Code to increment the team score - uses a php script
 //Written by: Nell
-function writeScore() {
+function writeScore(team_id, score_value) {
+	var xhttp;
+	xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
 
+	}
+	xhttp.open("GET", "score_write.php?t="+team_id+"s="+score_value, true);
+	xhttp.send();
 }
